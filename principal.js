ad6ffd27111db05ceca8f723d783d21cf58fa81e
Luis Alejandro Sardinas Chambilla <luisin_sardinas@hotmail.com>
2020-09-12 12:53:28 -0400

    first page ready

--- principal.js
+++ principal.js
@@ -1,18 +1,23 @@
 let fondoJuego;
-var juego = new Phaser.Game(1200, 650, Phaser.CANVAS, "bloque_juego");
+let boton;
+var juego = new Phaser.Game(1366, 657, Phaser.CANVAS, "bloque_juego");
 let rect;
 const estadoPricipal = {
   preload: function () {
     //Carga todos los recursos
     juego.load.image("fondo", "./assets/escenarios/nivel1.jpeg");
+    juego.load.image('rocket','./assets/personajes/Rocket.jpeg');
+    juego.load.image('btnPlay', './assets/interfaz/btn_play.png');
   },
+
 
   create: function () {
     //Mostrar en pantalle
-    fondoJuego = juego.add.tileSprite(0, 0, 1200, 650, "fondo", {
-      frameWidth: 100,
-      frameHeight: 100,
-    });
+    fondoJuego = juego.add.tileSprite(0, 0, 1366, 657, "fondo");
+    boton = juego.add.sprite(juego.width/2, juego.height/2, "btnPlay");
+    boton.anchor.setTo(.5, .5)
+    
+    //juego.add.sprite(0, 0, "rocket");
     //fondoJuego.scale.setTo(2.5, 1.7);
     // rect = new Phaser.Rectangle(0, 0, 1200, 650);
     // fondoJuego.alignIn(rect, Phaser.TOP_LEFT);
